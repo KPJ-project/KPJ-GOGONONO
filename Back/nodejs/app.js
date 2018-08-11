4f6db80c8ef0d5ee5de24f3609139f36385266ba
thkwon <TaeHYoungKwon@github.com>
2018-08-11 16:32:12 +0900

    test eb

--- Back/nodejs/app.js
+++ Back/nodejs/app.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var YAML = require('yamljs');
-var swaggerUi = require('swagger-ui-express');
-var swaggerDocument = YAML.load('./swagger.yaml');
-var app = express();
+var express = require("express")
+var YAML = require("yamljs")
+var swaggerUi = require("swagger-ui-express")
+var swaggerDocument = YAML.load("./swagger.yaml")
+var app = express()
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
-app.set('port',3000);
+app.set("port", 3000)
 
-app.get('/', function(req, res) {
-    res.send('GOGONONO API SERVER!');
-});
+app.get("/", function(req, res) {
+  res.send("GOGONONO API SERVER!")
+})
 
 // 모임 관련 api
-app.use('/groups', require('./routes/groups'));
+app.use("/groups", require("./routes/groups"))
 
 // 유저 관련 api
-app.use('/users', require('./routes/users'));
+app.use("/users", require("./routes/users"))
 
-app. listen(app.get('port'), function(){
-    console.log('gogonono express server listening on port ' + app.get('port'));
-});
+app.listen(app.get("port"), function() {
+  console.log("gogonono express server listening on port " + app.get("port"))
+})

a889737ef9959775a5b1f82d5bcbec074cc2ab1c
psw221 <tpdnddlek@gmail.com>
2018-08-16 09:38:53 +0900

    refactor: 로그인 할 때 post -> get으로 변경

--- Back/nodejs/routes/users.js
+++ Back/nodejs/routes/users.js
@@ -17,6 +17,56 @@ router.get('/', function(req, res){
     });
 });
 
+router.get('/login/:email', function(req, res){
+    console.log(req.params.email);
+    var email = req.params.email;
+
+    var selectQuery = "select u.RawID as 'userId', " +
+                    "  		 kg.RawID as 'groupId', " +
+                    "        kg.GroupName as 'groupName', " +
+                    "        (select count(*) " +
+                    "        from GroupJoin " +
+                    "        where GroupID = kg.RawID) as 'groupUserCount', " +
+                    "        (select count(*) " +
+                    "        from Votes " +
+                    "        where GroupID = kg.RawID and IsComplete = 1) as 'completeVotes', " +
+                    "        (select count(*) " +
+                    "        from Votes " +
+                    "        where GroupID = kg.RawID and IsComplete = 0) as 'notCompleteVotes', " +
+                    "        u.RegDate as 'regDate', " +
+                    "        u.LastLoginDate as 'lastLoginDate' " +
+                    "from Users as u " +
+                    "left outer join GroupJoin as gj on u.RawID = gj.UserID " +
+                    "left outer join KP_Groups as kg on gj.GroupID = kg.RawID " +
+                    "left outer join Votes as v on v.GroupID = kg.RawID " +
+                    "where u.EMail = '" + email+ "' " +
+                    "group by userId, groupId, groupName, groupUserCount, completeVotes, notCompleteVotes;";
+
+    conn.query(selectQuery, function(err, rows) {
+        if(err){
+            console.log(selectQuery);
+            res.send(err);
+        }
+
+        console.log(rows[0].userId + " " + rows[0].regDate+ " " + typeof(rows[0].lastLoginDate));
+        var isfirst;
+        if(rows[0].regDate.toString() == rows[0].lastLoginDate.toString()) {
+            isfirst = true;
+        }
+        else {
+            isfirst = false;
+        }
+
+        if(rows[0].groupName != null) {
+            res.send(ResultSuccess(isfirst, rows[0].userId, GetResult(rows)));
+        }
+        else {
+            res.send(ResultSuccess(isfirst, rows[0].userId, null));
+        }
+        
+    });
+});
+
 router.post('/login/', function(req, res){
     console.log(req.body.email);
     var email = req.body.email;
